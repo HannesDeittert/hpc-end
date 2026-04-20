c927c420248a4d78d9f41ae92caf05c4e0fc9675
Hannes Deittert <hannesdeittert@gmail.com>
2026-04-20 08:06:33 +0200

    simple tool versions for training on crusher (jshaped tool types) and check if identical to corresponding model in wire registry

---
+++ steve_recommender/bench/custom_tools_steve_straight_simple.py
@@ -0,0 +1,30 @@
+import eve
+
+BASELINE_TIP_LENGTH_MM = 15.205308443374598
+STRAIGHT_TIP_RADIUS_MM = 1000.0
+
+
+class JShapedDefaultStraightSimple(eve.intervention.device.JShaped):
+    def __init__(self):
+        super().__init__(
+            name="guidewire_straight",
+            velocity_limit=(35.0, 3.14),
+            length=450.0,
+            tip_radius=STRAIGHT_TIP_RADIUS_MM,
+            tip_angle=BASELINE_TIP_LENGTH_MM / STRAIGHT_TIP_RADIUS_MM,
+            tip_outer_diameter=0.7,
+            tip_inner_diameter=0.0,
+            straight_outer_diameter=0.89,
+            straight_inner_diameter=0.0,
+            poisson_ratio=0.49,
+            young_modulus_tip=17000.0,
+            young_modulus_straight=80000.0,
+            mass_density_tip=2.1e-05,
+            mass_density_straight=2.1e-05,
+            visu_edges_per_mm=0.5,
+            collis_edges_per_mm_tip=2.0,
+            collis_edges_per_mm_straight=0.1,
+            beams_per_mm_tip=1.4,
+            beams_per_mm_straight=0.5,
+            color=(0.0, 0.0, 0.0),
+        )
